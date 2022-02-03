02814e812d8d02d6a9df80fdfd879249acd02ef7
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2022-02-03 23:54:47 +0530

    Tutorials Updated

--- index.js
+++ index.js
@@ -1,123 +1,196 @@
-const mongoose = require('mongoose');
+    // * Content :
+    // * Schema Decalaration
+    // * Collection Creation
+    // * Document Creation or insertion
+    // * Read All Data of DB 
+    // * MongoDB Querying Methods
+    // * Sorting in MongoDB
+    // * Updation of field inside the Documents in MongoDB
+    // * Deletion of Documents inside collection
 
-// * Connecting mongoose to MongoDB
-mongoose.connect("mongodb://localhost:27017/MyData", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Successful"))
-    .catch((err) => console.log(err));
 
 
-// * Schema Decalaration
-const detailsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: Number,
-    love4Coding: Boolean,
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+    
+    const express = require('express');
+    const mongoose = require('mongoose');
 
-// * Collection Creation
-const Details = new mongoose.model('Detail', detailsSchema);
+    // * Connecting mongoose to MongoDB
+    mongoose.connect("mongodb://localhost:27017/MyData", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log("Successful"))
+        .catch((err) => console.log(err));
 
-// * Document Creation or insertion
 
-// * One at a time
+    // 1    ***************************************************************************   //
 
-const createDocument = async () => {
-    const user1 = new Details(
-        {
-            name: "Albert",
-            love4Coding: false
+    // * Schema Decalaration
+    const detailsSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        age: Number,
+        love4Coding: Boolean,
+        date: {
+            type: Date,
+            default: Date.now
         }
-    );
-
-    const result = await user1.save(); // * .save method IMP
-    console.log(result);
-}
-
-// createDocument();
-
-// * Many at a time
-
-// const createManyDocuments = async () => {
-
-//     try {
-//         const user2 = new Details(
-//             {
-//                 name: "Mohit Chauhan",
-//                 age: 20,
-//                 love4Coding: true
-//             }
-//         );
-
-//         const user3 = new Details(
-//             {
-//                 name: "Chauhan Sahab",
-//                 age: 20,
-//                 love4Coding: true
-//             }
-//         );
-
-//         const user4 = new Details(
-//             {
-//                 name: "Sallu",
-//                 age: 60,
-//                 love4Coding: false
-//             }
-//         );
-
-//         const result = await Details.insertMany([user2, user3, user4]); // insertMany takes an array of users
-//         console.log(result);
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
-
-// createManyDocuments();
+    })
 
 
-const getDocument = async () => {
+    // 2    ***************************************************************************   //
 
-    // * we can use projections in either ways
-    // 1. Putting in find method...find(query,projection)
-    // const result = await Details.find({"age":"20"},{name:1,_id:0});
-
-    // 2. By using select method
-    // const result = await Details.find({ "age": "20" })
-    //     .select({ name: 1, _id: 0 })
-    //     .limit(1);
+    // * Collection Creation
+    const Details = new mongoose.model('Detail', detailsSchema);
 
 
-    // * Comparison Query Operators in MongoDB 
-    // Docs : https://docs.mongodb.com/manual/reference/operator/query-comparison/
-    // const result = await Details.find({age:{$lte:20}}); //gte,lte,gt,lt,eq,neq
+    // 3    ***************************************************************************   //
 
-    // The $in operator selects the documents where the value of a field equals any value in the specified array.
-    // const result = await Details.find({age:{$in : [23,69,60]}}); // in,nin
+    // * Document Creation or insertion
+
+    // ** One at a time
+    const createDocument = async () => {
+        const user1 = new Details(
+            {
+                name: "Albert",
+                love4Coding: false
+            }
+        );
+
+        const result = await user1.save(); // * .save method IMP
+        console.log(result);
+    }
+    // createDocument();
+
+    // ** Many at a time
+    const createManyDocuments = async () => {
+        try {
+            const user2 = new Details(
+                {
+                    name: "Mohit Chauhan",
+                    age: 20,
+                    love4Coding: true
+                }
+            );
+
+            const user3 = new Details(
+                {
+                    name: "Chauhan Sahab",
+                    age: 20,
+                    love4Coding: true
+                }
+            );
+
+            const user4 = new Details(
+                {
+                    name: "Sallu",
+                    age: 60,
+                    love4Coding: false
+                }
+            );
+
+            const result = await Details.insertMany([user2, user3, user4]); // insertMany takes an array of users
+            console.log(result);
+            
+            // * 2nd Method of InsertMany Without Async Await
+            // Details.insertMany([user2, user3, user4], (err)=>{
+            //     if(err) console.log(err);
+            //     else console.log("Success");
+            // })
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    // createManyDocuments();
+
+
+    // 4    ***************************************************************************   //
+
+    // * MongoDB Querying Methods
+
+    // * Read All Data of DB 
+    // * 1st Method (Without Async Await)
+    Details.find((err,result)=>{
+        if(err) console.log(err); // TODO : we can also run forEach loop for performing action on every element.
+        else console.log(result);
+    });
+
+    const getDocument = async () => {
+        
+        let result; // Just for convinence
+
+        // * 2nd Method (With Async Await)
+        result = await Details.find();
+
+
+
+        
+        // * we can use projections in either ways
+        // 1. Putting in find method...find(query,projection)
+        // result = await Details.find({"age":"20"},{name:1,_id:0});
+        
+        // 2. By using select method
+        // result = await Details.find({ "age": "20" })
+        //     .select({ name: 1, _id: 0 })
+        // .limit(1); // also a method .countDocuments()
+        
+        
+        // * Comparison Query Operators in MongoDB 
+        // Docs : https://docs.mongodb.com/manual/reference/operator/query-comparison/
+        // result = await Details.find({age:{$lte:20}}); //gte,lte,gt,lt,eq,neq
+        
+        // The $in operator selects the documents where the value of a field equals any value in the specified array.
+        // result = await Details.find({age:{$in : [23,69,60]}}); // in,nin
+        
+        
+        // * Logical Query Operators in MongoDB
+        // Docs : https://docs.mongodb.com/manual/reference/operator/query-logical/
+        // $and performs a logical AND operation on an array of one or more expressions
+        // result = await Details.find({$and: [{age:20},{love4Coding:true}]}); // and,or,nor,not
+        
+        // $not performs a logical NOT operation on the specified <operator-expression> and selects the documents that do not match the <operator-expression>. 
+        // IMP: This includes documents that do not contain the field.
+        // result = await Details.find({age:{$not: {$gt:25} }}); 
+        
+        
+        console.log(result); // here we get array of objects (documents)
+    }
+    // getDocument();
+
+
+    // 5    ***************************************************************************   //
+    
+    // * Sorting in MongoDB
+    // Docs : https://www.tutorialspoint.com/mongodb/mongodb_sort_record.htm
+    
+    const getSortedData = async () => {
+        const result = await Details.find().sort({age:1}); //
+        console.log(result);
+    }
+    
+    // getSortedData();
     
     
-    // * Logical Query Operators in MongoDB
-    // Docs : https://docs.mongodb.com/manual/reference/operator/query-logical/
-    // $and performs a logical AND operation on an array of one or more expressions
-    // const result = await Details.find({$and: [{age:20},{love4Coding:true}]}); // and,or,nor,not
+    // 6    ***************************************************************************   //
+    
+    //* Updation of field inside the Documents in MongoDB
+    
+    // Details.updateOne({name:"Mohit Chauhan"},{age:21}, (err)=>console.log(err));
+    // ( {item to update},{what to update in it}, Callback function(err) )
+    
+    
+    // 7    ***************************************************************************   //
+    // * Deletion of Documents inside collection
 
-    // $not performs a logical NOT operation on the specified <operator-expression> and selects the documents that do not match the <operator-expression>. 
-    // IMP: This includes documents that do not contain the field.
-    // const result = await Details.find({age:{$not: {$gt:25} }}); 
+    Details.deleteOne({name:"Mohit Chauhan"}, (err)=>{
+        if(err) console.log(err);
+        else console.log("Sucessfully Deleted");
+    })
 
-
-
-    console.log(result); // here we get array of objects (documents)
-}
-
-
-
-getDocument();
+    Details.find((err,result)=>{
+        if(err) console.log(err); // TODO : we can also run forEach loop for performing action on every element.
+        else console.log(result);
+    });
